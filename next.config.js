bfb9d9b1cfdbf5f33ec44513e9ec94df6ac80ef0
AndreiKravets <tetamoed1@gmail.com>
2021-11-09 16:42:06 +0200

    next-pwa

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
+const withPWA = require("next-pwa");
+const runtimeCaching = require('next-pwa/cache')
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    runtimeCaching,
+    register: true,
+    skipWaiting: true,
+  },
   reactStrictMode: false,
-}
+})
