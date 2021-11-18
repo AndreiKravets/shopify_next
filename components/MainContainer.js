f2dda2e8ebb5da3aec7dae17c88637241193b5db
AndreiKravets <tetamoed1@gmail.com>
2021-11-18 17:51:08 +0200

    slick-slider

--- components/MainContainer.js
+++ components/MainContainer.js
@@ -9,6 +9,8 @@ export default function MainContainer({children, title}){
             <Head>
                 <meta keywords="shopify next"></meta>
                 <title>Shopify  {title}</title>
+                <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"/>
+                <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"/>
             </Head>
             <div id="root">
                     <Header/>
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,84 @@
 import MainContainer from "../components/MainContainer";
+import {shopifyClient} from "../utils/shopify";
+import Prismic from "@prismicio/client";
+import Slider from "react-slick";
+import Link from "next/link";
+import React from "react";
 
-export default function Home() {
+
+export default function Home({collections,homepage,slider}) {
+    console.log(collections)
+    console.log(homepage)
+    console.log(slider)
+    homepage = homepage.results[0].data
+    slider = slider.results
+
+        var settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        }
+
   return (
     <MainContainer>
         <div className="container-fluid home_top_section">
-            <img src="/homebaner.jpg" alt="home"/>
             <div className="container">
-        <h1>Grand Opening</h1>
-            <p>Any order <span>over $500</span> receives official <br/> <span>Sikacci vintage tee</span></p>
+                <Slider {...settings}>
+            {slider.map((slide,index) => {
+                return(
+
+                    <div className="row" key={index}>
+                        <div className="col-md-6">
+                            <h1>{slide.data.title[0].text}</h1>
+                            {slide.data.subtitle.map((paragraph, index) => {
+                                return (
+                                    <p key={index}>{paragraph.text}</p>
+                                )
+                            })}
+                        </div>
+                        <div className="col-md-6">
+                            <img src={slide.data.big_image.url} />
+                            <img src={slide.data.small_image.url} />
+                        </div>
+                    </div>
+
+                )
+
+
+            })}
+                </Slider>
             </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            {collections.map((product, index) => {
+              return (
+                <div className="col-md-4" key={index}>
+                    <div style={{backgroundImage: `url(${product.image.src})`}}>
+                        <Link href={`/collections/${index}`}>{product.handle}</Link>
+                        <h5>Look More</h5>
+                    </div>
+                </div>
+              )
+             })
+            }
+          </div>
         </div>
     </MainContainer>
   )
+}
+
+export async function getServerSideProps() {
+    const collections = await shopifyClient.collection.fetchAllWithProducts()
+    const client = Prismic.client("https://paspartoo.prismic.io/api/v2", {})
+    const homepage = await client.query(Prismic.Predicates.at('document.type', 'homepage'))
+    const slider = await client.query(Prismic.Predicates.at('document.type', 'home_slider'))
+
+    return { props: {
+            collections:JSON.parse(JSON.stringify(collections)),
+            homepage: homepage,
+            slider: slider
+        }};
 }
