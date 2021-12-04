66598509039861897d0a2605a19a9dcbdd6ff3b5
AndreiKravets <tetamoed1@gmail.com>
2021-12-04 17:38:02 +0200

    fix build error

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import '../styles/bootstrap.css';
 import '../styles/global.scss';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
